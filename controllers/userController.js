65430cb1147156253f6ee455f7868f597b5302cb
chrisdmar <chrisdmar12@gmail.com>
2023-01-03 22:33:26 +0100

    user crud ok

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,7 @@
-const {UserModel} = require('../models/userModel');
+const  {UserModel} = require("../models/userModel");
 const ObjectId = require('mongoose').Types.ObjectId;
+
+
 
 module.exports.getAllUsers = async (req, res) => {
     // select('-password) selectionne tous sauf le password
@@ -18,34 +20,45 @@ module.exports.userInfo = (req, res) => {
     }).select('-password');
 };
 
+
+
 module.exports.updateUser = async (req, res) => {
     if (!ObjectId.isValid(req.params.id)) return res.status(400).send('ID unknow : ' + req.params.id);
-
     try {
-        await UserModel.findOneAndUpdate(
-            {
+        UserModel.findOneAndUpdate({
                 _id: req.params.id
-            }, 
-            {
+            }, {
                 $set: {
                     picture: req.body.picture
-                }
-            }, 
-            {
+                },
+            }, {
                 new: true,
                 upsert: true,
                 setDefaultsOnInsert: true
-            },{},
+            },
             (err, docs) => {
-                console.log(docs);
-                if (!err) return res.status(200).send({docs});
-                if (err) return res.status(501).send({
+                if (!err) return res.send(docs);
+                if (err) return res.status(500).send({
                     message: err
                 });
             }
-            )
+        );
+    } catch (err) {
+        return res.status(500).json({
+            message: err
+        });
     }
-    catch (err) {
-        return res.status(500).json({message: err})
+}
+
+
+module.exports.deleteUser = async (req, res) => {
+    if (!ObjectId.isValid(req.params.id)) 
+    return res.status(400).send('ID unknow : ' + req.params.id);
+
+    try {
+        await UserModel.deleteOne({_id: req.params.id})
+            res.status(200).json({message: "Suppression réussi."});
+    } catch (err) {
+        return res.status(500).json({message: "kkk"});
     }
 }
