c6b752e27fac4c75705004cc97e4ea9ba46753d3
chrisdmar <chrisdmar12@gmail.com>
2023-03-10 18:09:50 +0100

    add user search

--- client/src/components/Admin/Users.js
+++ client/src/components/Admin/Users.js
@@ -4,19 +4,40 @@ import { useEffect, useState } from 'react';
 export default function Users() {
 
   const [data, setData] = useState(null);
+  const [searchData, setSearchData] = useState(data);
+  const [search, setSearch] = useState("");
+
 
   useEffect(() => {
-    fetch("https://crm-back.vercel.app/customers")
+    fetch("https://crm-back.vercel.app/customers", [{mode:'no-cors'}])
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((data) => {
+        setData(data); 
+      });
   }, []);
 
 
+  
+  // useEffect(() => {
+  //   setSearchData(() => {
+  //     return data.filter((user) => {
+  //       return user.firstname.toLowerCase().includes(search.toLowerCase());
+  //     });
+  //   });
+  // }, [search]); // <- add the count variable here
 
 
   return (
     <div id='users'>
     <h1>Liste des utilisateurs</h1>
+
+    <input 
+    type="text" 
+    className='user__search'
+    placeholder="Rechercher un utilisateur" 
+    value={search}
+    onChange={(e)=>{setSearch(e.target.value)}}
+    />
 
     <table>
       <thead>
@@ -38,16 +59,16 @@ export default function Users() {
     {
       data && data.map((user, index) => (
         <tr key={user._id}>
-          <td>{index + 1}</td>
-          <td>{user.lastname}</td>
-          <td>{user.firstname}</td>
-          <td><button className='user__btn'><a href={"mailto:" + user.email}>Envoyer</a></button></td>
-          <td><button className='user__btn'><a href={`tel:${user.phone}`}>Appeler</a></button></td>
-          <td>{user.address}</td>
-          <td>{user.cp}</td>
-          <td>{user.city}</td>
-          <td><button className='user__btn'><a href={`https://www.google.fr/maps/place/${user.address},+${user.cp}+${user.city}`} target="_blank" rel="noreferrer">Aller</a></button></td>
-          <td><button className='user__btn'><a href={`/details/${user._id}`}>Voir</a></button></td>
+          <td data-label='Numéro'>{index + 1}</td>
+          <td data-label='Nom'>{user.lastname}</td>
+          <td data-label='Prénom'>{user.firstname}</td>
+          <td data-label='Email'><button className='user__btn'><a href={"mailto:" + user.email}>Envoyer</a></button></td>
+          <td data-label='Téléphone'><button className='user__btn'><a href={`tel:${user.phone}`}>Appeler</a></button></td>
+          <td data-label='Adresse'>{user.address}</td>
+          <td data-label='Code Postal'>{user.cp}</td>
+          <td data-label='Ville'>{user.city}</td>
+          <td data-label='Itinéraire'><button className='user__btn'><a href={`https://www.google.fr/maps/place/${user.address},+${user.cp}+${user.city}`} target="_blank" rel="noreferrer">Aller</a></button></td>
+          <td data-label='Détails'><button className='user__btn'><a href={`/details/${user._id}`}>Voir</a></button></td>
         </tr>
       ))
     }
